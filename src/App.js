269e3ff64fe1a09bf740ada4d3ce642438fe2bf6
tharindi22 <ttharindiattanayaka@gmail.com>
2024-12-23 10:19:38 +0530

    modified files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-const port = process.env.PORT || 3001; // Change 3001 if needed
-App.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <main className='text-lg text-red-500 font-bold'>
-      React App
+    
+    <main >
+      <Outlet/>
+      
     </main>
   );
 }
