9a64e219deeadc8e7cf16d871e5a9bad69cf69ec
tharindi22 <ttharindiattanayaka@gmail.com>
2024-12-24 22:46:04 +0530

    Updates for Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import { NavLink } from 'react-router-dom'
+import userIcon from '../assets/user.png'
 
 const Header = () => {
 
@@ -19,8 +20,8 @@ const Header = () => {
     
     <header className='fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75'>
         <div className='container mx-auto px-3 flex items-center h-full'>
-          <div className=''>
-            <img src={logo} alt='logo' width={120} />
+          <div className='w-150 h-150'>
+            <img src={logo} alt='logo' width={150}  />
           </div>
 
           <nav className='hidden lg:flex items-center gap-1 ml-5'>
@@ -36,6 +37,17 @@ const Header = () => {
                 })
               }
           </nav>
+
+
+          <div className="ml-auto">
+            <div></div>
+            <div className="w-10 h-10 rounded-full overflow-hidden cursor-pointer active:scale-50 transition-all">
+              <img src={userIcon} alt="user" className="w-full h-full" />
+            </div>
+          </div>
+
+
+
         </div>
 
     </header>
